5c55a0aef4785867e931e0f9a08fce69e6edb8ed
Michael Cote <michaelcote7@outlook.com>
2020-06-05 17:25:25 -0700

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,27 @@ import Create from './components/Create';
 import Update from './components/Update';
 import Header from './components/Header';
 import Clock from './components/Clock';
+// import BIRDS from 'vanta/dist/vanta.birds.min';
 
 // change clock to footer component
 
+// const birds = BIRDS({
+//   el: ".myheader",
+//   birdSize: 6.50,
+//   wingSpan: 26.00,
+//   speedLimit: 4.00,
+//   separation: 32.00,
+//   alignment: 33.00,
+//   cohesion: 33.00,
+//   quantity: 2.00
+// });
+
+
 function App() {
+  
   return (
     <div className="App">
+      <div id="backg"></div>
       <Header />
       <Router>
         <AuthorsMain path="/" />
